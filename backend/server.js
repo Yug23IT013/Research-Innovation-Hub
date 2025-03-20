5524cbc591cdf03675a293aa5f29bd6d18c65b97
krishna23IT017 <152592270+krishna190806@users.noreply.github.com>
2025-03-20 20:44:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-server
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+require("dotenv").config();
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
